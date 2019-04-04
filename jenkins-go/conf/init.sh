1d615f00c566d82a6730a9f4166688ae97cddc00
chenli <chenli@haituncun.com>
2019-04-04 09:29:26 +0800

    jenkins go

--- jenkins-go/conf/init.sh
+++ jenkins-go/conf/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/sbin/sshd 
-su - deploy -c " cd /usr/lib/jenkins && java -jar jenkins.war > /tmp/enkins.txt"
+su - deploy -c " cd /usr/lib/jenkins && java -jar jenkins.war > /tmp/jenkins.txt"
