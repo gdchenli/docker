b6338c2378d6849cbd7444cb81d702f32107c684
cl <cl@cldeMacBook-Pro.local>
2019-09-25 11:23:33 +0800

    php

--- openresty-php5.6/conf/init.sh
+++ openresty-php5.6/conf/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #/usr/sbin/sshd -D
 /usr/sbin/sshd  #启动ssh
+/usr/sbin/php-fpm #启动php-fpm
 /usr/local/openresty/nginx/sbin/nginx -p /usr/local/openresty/nginx/  -g 'daemon off;' #启动nginx
